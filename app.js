dadfc9f8b25f6ebadd2326d9569dd124f1e9279c
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-01-15 00:28:28 -0300

    search and display results done

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-const findCoords = (address => {
-  const url = `https://nominatim.openstreetmap.org/search?street=${address}&city=Rio%20de%20Janeiro&format=json&polygon=1&addressdetails=1`;
-  let coords = [];
-  fetch(url)
+const btn = document.getElementById("button");
+
+btn.addEventListener("click",(event) => {
+  event.preventDefault();
+  const address = document.getElementById("address-input").value;
+  fetch(`https://nominatim.openstreetmap.org/search?street=${address}&city=Rio%20de%20Janeiro&format=json&polygon=1&addressdetails=1`)
     .then(response => response.json())
     .then((data) => {
-      coords[0] = Number.parseFloat(data[0].lon)
-      coords[1] = Number.parseFloat(data[0].lat);
-    });
-  return coords;
-});
-
-
-const distance = (coordsArray => {
-  fetch("https://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/estacoesBikeRio")
-    .then(response => response.json())
-    .then((data) => {
-      const results = data.DATA;
-      const coordsBike = coordsArray;
-
+      const coords = [Number.parseFloat(data[0].lon), Number.parseFloat(data[0].lat)];
       let dists = [];
+      fetch("https://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/estacoesBikeRio")
+        .then(response => response.json())
+        .then((data) => {
+          const results = data.DATA;
+          results.forEach(element => {
+            euclidDist = coords[0];
+            dists.push([element[3], element[4], element[0], element[6], element[5], Math.sqrt((coords[0] - Number.parseFloat(element[6])) ** 2 + (coords[1] - Number.parseFloat(element[5])) ** 2)]);
+          });
+          const distsSorted = dists.sort((a, b) => a[5] - b[5]);
+          //PAREI AQUI
+          const resultDiv = document.getElementById("results");
+          resultDiv.insertAdjacentHTML("beforeend", `<h5><i class="logo fas fa-bicycle text-primary"></i> Os 5 bicicletários mais próximos</h5>`)
+          distsSorted.slice(0,4).forEach(element => {
+            resultDiv.insertAdjacentHTML("beforeend",`<p class="text-secondary">${element[0]} ${element[1]}, ${element[2]}</p>`)
 
-      for (var i = 0; i < results.length; i++) {
-        let euclidDist = Math.sqrt((coordsBike[0] - Number.parseFloat(results[i][6]))**2 + (coordsBike[1] - Number.parseFloat(results[i][5]))**2);
-        console.log(euclidDist)
-        dists[i] = [results[i][3], results[i][4], results[i][0], results[i][6], results[i][5], euclidDist];
-      };
+          })
 
-      const distsSorted = dists.sort((a, b) => parseFloat(b[5]) - parseFloat(a[5]));
-      console.log(dists);
+        });
     });
 });
-
-
-const a = findCoords("Bangu");
-distance(a)
